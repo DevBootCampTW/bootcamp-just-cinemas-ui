49bcdec64de90cae9d4965d843adf8a2489cab2f
Gaurang Agarwal <agaurang@INagaurang.local>
2018-07-17 18:22:00 +0530

    Add Style for Movie Listing

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -12,17 +12,17 @@ export class MovieItem extends Component {
         return (
             <div className="card-movie">
                 <img src={this.props.movie['poster']} className="card-img-top" 
-                    style={{width:150+'px',height:200+'px', cursor: "pointer"}} 
+                    style={{width:200+'px',height:200+'px', cursor: "pointer"}}
                     onClick={()=>this.props.history.push(`/movie/${this.props.movie.imdbID}`)}
                 />
-                <div className="container-movie" style={{width:150+'px'}}>
-                    <h5 className="card-title-movie " style={{cursor: "pointer"}}
+                <div className="container-movie">
+                    <p className="card-title-movie " style={{cursor: "pointer"}}
                         onClick={()=>this.props.history.push(`/movie/${this.props.movie.imdbID}`)}>
                         {this.props.movie['title']}
-                    </h5>
-                    <div>
-                    <div className="card-text-movie">{this.props.movie['soundEffects'].join(' | ')}</div>
-                    <div className="card-text-movie text-muted">IMDB Rating : {this.props.movie['imdbRating']}</div>
+                    </p>
+                    <div className="card-text-wrapper">
+                    <div className="card-text-movie" style={{float:'left'}}>{this.props.movie['soundEffects'].join(' | ')}</div>
+                    <div className="card-text-movie text-muted" style={{float:'right'}}> {this.props.movie['imdbRating']}/10</div>
                     </div>
                 </div>
             </div>
