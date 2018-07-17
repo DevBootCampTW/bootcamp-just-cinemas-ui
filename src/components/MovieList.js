904068c3795ce05e873dc447b7ff0c901032e0f5
Vaibhawk <vaibhawk@in-vaibhawk.local>
2018-07-17 18:55:01 +0530

    1 | arun & vaibhaw | Fix test

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,25 +1,18 @@
-import React, {Component} from 'react';
-import {requestMovieList} from '../containers/MovieList'
+import React, { Component } from 'react';
 import MovieItem from './MovieItem'
 
 class MovieList extends Component {
 
     componentDidMount() {
-        console.log('check if its even here')
         this.props.requestMovieList();
-
     }
 
     render() {
-        return this.showMovies();
-    }
-
-    showMovies() {
         return (
             <div className="row">
-            {
-                this.props.movies.map((movie) => <MovieItem movie = {movie}/>)
-            }
+                {
+                    this.props.movies.map((movie) => <MovieItem movie={movie} />)
+                }
             </div>
         );
     }
