f9a7c42d5fea52fe7b1b0435dd2c8a5bbe497c4b
PremnishaTW <premniba@thoughtworks.com>
2018-07-18 15:59:28 +0530

    Feedback Move anonymous fns | Arun & Premnisha

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { withRouter } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
 export class MovieItem extends Component {
 
@@ -8,26 +8,26 @@ export class MovieItem extends Component {
         return this.showMovies();
     }
 
-    onClickOfMovie(imdbId)
-    {
-        this.props.history.push(`/movie/${imdbId}`);
-    }
+    onMovieClick = () => this.props.history.push(`/movie/${this.props.movie.imdbID}`);
 
     showMovies() {
         return (
             <div className="card-movie">
-                <img src={this.props.movie['poster']} className="card-img-top" 
-                    style={{width:200+'px',height:200+'px', cursor: "pointer"}}
-                    onClick={this.onClickOfMovie(this.props.movie.imdbID)}
+                <img src={this.props.movie['poster']} className="card-img-top"
+                     style={{width: 200 + 'px', height: 200 + 'px', cursor: "pointer"}}
+                     onClick={this.onMovieClick}
                 />
                 <div className="container-movie">
                     <p className="card-title-movie " style={{cursor: "pointer"}}
-                        onClick={this.onClickOfMovie(this.props.movie.imdbID)}>
+                       onClick={this.onMovieClick}>
                         {this.props.movie['title']}
                     </p>
                     <div className="card-text-wrapper">
-                    <div className="card-text-movie" style={{float:'left'}}>{this.props.movie['soundEffects'].join(' | ')}</div>
-                    <div className="card-text-movie text-muted" style={{float:'right'}}> {this.props.movie['imdbRating']}/10</div>
+                        <div className="card-text-movie"
+                             style={{float: 'left'}}>{this.props.movie['soundEffects'].join(' | ')}</div>
+                        <div className="card-text-movie text-muted"
+                             style={{float: 'right'}}> {this.props.movie['imdbRating']}/10
+                        </div>
                     </div>
                 </div>
             </div>
